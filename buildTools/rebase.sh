3081ec11958160a7cd9d46aecdbe2a77e7da80b8
dangherve <dangherve@gmail.com>
2026-03-12 13:44:08 -0400

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git remote add upstream https://github.com/asophila/Flash-CHIP.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/Flash-CHIP.git
+git push --force --set-upstream origin master
